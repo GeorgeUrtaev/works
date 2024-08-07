f8c72ea2da7a04bd9638e70031e96fddc4294260
GeorgeUrtaev <shpanka1996@gmail.com>
2024-08-07 13:46:24 +0300

    добавил 2 проекта 1. личный кабинет 2. popup

---
+++ учеба/pop-up неадеквата/script.js
@@ -0,0 +1,10 @@
+function show(teg)
+{
+	let qwe = document.querySelector(teg)
+	qwe.classList.add('active')
+}
+function remove(teg)
+{
+	let qwe = document.querySelector(teg)
+	qwe.classList.remove('active')
+}
